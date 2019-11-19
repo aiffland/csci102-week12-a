efff24b5a164100b187adad8e35fc57fab9e32c6
aiffland <57680433+aiffland@users.noreply.github.com>
2019-11-19 09:10:09 -0700

    Add files via upload

--- Week12-utility.py
+++ Week12-utility.py
@@ -4,3 +4,14 @@
 
 def PrintOutput(string):
     return print("OUTPUT", string)
+
+def LoadFile(string):
+    tot_list=[]
+    with open(string, 'r') as f:
+        for line in f:
+            tot_list.append(line)
+
+    return tot_list
+
+    
+            
